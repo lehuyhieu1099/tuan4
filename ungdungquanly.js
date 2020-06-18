19f5e95e824473da88b821c6214d7997c77ea352
huyhieu <lehuyhieu1099@gmail.com>
2020-06-18 15:26:03 +0700

    Ham

---
+++ ungdungquanly.js
@@ -0,0 +1,10 @@
+let mang = ['Sony Xperia', 'Samsung Galaxy', 'Nokia 6', 'Xiaomi Redmi Note 4', 'Apple iPhone 6S', 'Xiaomi 5s Plus', 'Apple iPhone 8 Plus', 'Fujitsu F-04E', 'Oppo A71']
+
+function hienThiSanPham() {
+    bang = `<table border='1' width='300' cellspacing='0' cellpadding='3'>`
+    for (i = 0; i <= mang.length; i++) {
+        bang = bang + "<tr>";
+
+
+    }
+}
